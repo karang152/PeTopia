9efa12d073087ba2998e5a98c5a21f667662511f
Nitin Kumar <106958535+nitink3006@users.noreply.github.com>
2025-01-25 21:50:28 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -51,7 +51,7 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("Connected to DB");
-    const PORT = 9000;
+    const PORT = process.env.PORT || 9000;
     app.listen(PORT, () => {
       console.log(`Listening on port ${PORT}`);
     });
